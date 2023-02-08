231b1c3e4ded36c1662975596bbcd41ce8bb602f
coherent17 <mnb51817@gmail.com>
2023-02-08 20:54:41 +0800

    finish p3

--- HW0/p3/p3.cpp
+++ HW0/p3/p3.cpp
@@ -33,7 +33,7 @@ void parser(char *filename, vector <cell> &cells, vector <int> &entrys){
             cell temp;
             temp.layer = layer;
             temp.id = cell_count + i;
-            temp.cost = state[i] == 0 ? 99999 : state[i];
+            temp.cost = state[i] == 0 ? INT_MAX : state[i];
             temp.layer_size = int(state.size());
 
             if(i == 0){
@@ -98,12 +98,7 @@ void findVerticalNeighbor(vector <cell> &cells, vector <int> entrys){
     for(size_t i = entrys.size() - 1; i > 0; i--){
         int layer1_index = entrys[i];
         int layer2_index = entrys[i - 1];
-
-        //cout << layer1_index << " " << layer2_index << endl;
-
         int layer2_push_back_index = layer2_index;
-
-        cout << cells[layer1_index].layer_size << " " << cells[layer2_index].layer_size << endl;
 
         if(cells[layer1_index].layer_size != cells[layer2_index].layer_size){
             for(int j = layer1_index; layer2_push_back_index != layer1_index; j = j + 2){
@@ -130,7 +125,9 @@ void Dijkstra(vector <cell> cells, vector <int> &distance, vector <int> &parent,
         visited[curr] = true;
         vector<int> currNeighbors = cells[curr].neighbors;
         for(size_t i = 0; i < currNeighbors.size(); i++){
-            if(distance[curr] + cells[currNeighbors[i]].cost < distance[currNeighbors[i]]){
+            unsigned long long int new_cost = distance[curr] + cells[currNeighbors[i]].cost;
+            unsigned long long int old_cost = distance[currNeighbors[i]];
+            if(new_cost < old_cost){
                 distance[currNeighbors[i]] = distance[curr] + cells[currNeighbors[i]].cost;
                 parent[currNeighbors[i]] = curr;
             }
@@ -144,26 +141,26 @@ void Dijkstra(vector <cell> cells, vector <int> &distance, vector <int> &parent,
             }
         }
     }
+
 }
 
 int findExit(vector <cell> cells, vector <int> entrys){
     int start_index = entrys[entrys.size() - 1];
-    cout << start_index << " " << cells[start_index].id + cells[start_index].layer_size << endl;
 
     for(int i = start_index; i < cells[start_index].id + cells[start_index].layer_size; i++){
-        if(cells[i].cost != 99999) return i;
+        if(cells[i].cost != INT_MAX) return i;
     }
     
     return -1;
 }
 
-void printPath(vector <int> parent, int exit_index){
+void getPath(vector <int> parent, int exit_index, vector <int> &path){
     if(parent[exit_index] == -1){
-        cout << exit_index << " ";
+        path.push_back(exit_index);
         return;
     }
-    printPath(parent, parent[exit_index]);
-    cout << exit_index << " ";
+    getPath(parent, parent[exit_index], path);
+    path.push_back(exit_index);
 }
 
 
@@ -171,23 +168,41 @@ int main(int argc, char *argv[]){
     vector <cell> cells;    //adjacency list
     vector <int> entrys;
     parser(argv[1], cells, entrys);
-    //printCells(cells);
 
     findVerticalNeighbor(cells, entrys);
-    printCells(cells);
 
+    vector<vector<int>> distance(6, vector<int>(cells.size(), INT_MAX));
+    vector<vector<int>> parent(6, vector<int>(cells.size(), -1));
 
-    vector <int> distance(cells.size(), INT_MAX);
-    vector <int> parent(cells.size(), -1);
-    Dijkstra(cells, distance, parent, 1);
-    
+    for(int i = 0; i < 6; i++){
+        if(cells[i].cost != INT_MAX) Dijkstra(cells, distance[i], parent[i], i);
+    }
 
     //find the exit
     int exit_index = findExit(cells, entrys);
-    cout << exit_index << endl;
-    cout << distance[exit_index] << endl;
 
-    printPath(parent, exit_index);
-    cout << endl;
+    int min_index = 0;
+    int min_distance = INT_MAX;
+    for(int i = 0; i < 6; i++){
+        if(distance[i][exit_index] < min_distance){
+            min_index = i;
+            min_distance = distance[i][exit_index];
+        }
+    }
+
+    if(min_distance == INT_MAX){
+        cout << "fail" << endl;
+    }
+    else{
+        vector <int> path;
+        getPath(parent[min_index], exit_index, path);
+
+        for(size_t i = 0; i < path.size(); i++){
+            cout << path[i];
+            if(i != path.size() - 1) cout << " -> ";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
